2f7e94f7896e3f86274171bd5f83ae5271f3b1c4
WAL <a.a.hamam.93@gmail.com>
2021-06-17 09:16:55 -0400

    First Version Fixed

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,7 +9,7 @@ protocol WeatherManagerDelegate {
 }
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=e72ca729af228beabd5d20e3b7749713&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=\(Key.myKey)&units=metric"
     
     var delegate: WeatherManagerDelegate?
     
